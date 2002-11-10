6002952a1ea8994bed1f887cd0edf89d3d338ac4
(no author) <(no author)@387891d4-d844-0410-90c0-e4c51a9137d3>
2002-11-10 20:40:03 +0000

    This commit was manufactured by cvs2svn to create tag 'fs2_open_3_3'.
    git-svn-id: svn://svn.icculus.org/fs2open@162 387891d4-d844-0410-90c0-e4c51a9137d3

---
+++ tags/fs2_open_3_3/fs2_open/code/missionui/missionpause.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) Volition, Inc. 1999.  All rights reserved.
+ *
+ * All source code herein is the property of Volition, Inc. You may not sell 
+ * or otherwise commercially exploit the source or things you created based on the 
+ * source.
+ *
+*/ 
+
+/*
+ * $Logfile: /Freespace2/code/MissionUI/MissionPause.h $
+ * $Revision: 2.2 $
+ * $Date: 2002-10-19 03:50:29 $
+ * $Author: randomtiger $
+ * 
+ *
+ * $Log: not supported by cvs2svn $
+ * Revision 2.1  2002/08/01 01:41:07  penguin
+ * The big include file move
+ *
+ * Revision 2.0  2002/06/03 04:02:25  penguin
+ * Warpcore CVS sync
+ *
+ * Revision 1.1  2002/05/02 18:03:10  mharris
+ * Initial checkin - converted filenames and includes to lower case
+ *
+ * 
+ * 3     6/09/99 2:17p Dave
+ * Fixed up pleasewait bitmap rendering.
+ * 
+ *
+ * $NoKeywords: $
+ */
+
+#ifndef _MISSION_PAUSE_HEADER_FILE
+#define _MISSION_PAUSE_HEADER_FILE
+
+#include "graphics/2d.h"
+
+// ----------------------------------------------------------------------------------------------------------------
+// PAUSE DEFINES/VARS
+//
+
+// pause bitmap display stuff
+extern int Please_wait_coords[GR_NUM_RESOLUTIONS][4];
+
+
+// ----------------------------------------------------------------------------------------------------------------
+// PAUSE FUNCTIONS
+//
+
+// initialize the pause screen
+void pause_init(int multi);
+
+// pause do frame - will handle running multiplayer operations if necessary
+void pause_do(int multi);
+
+// close the pause screen
+void pause_close(int multi);
+
+// debug pause init
+void pause_debug_init();
+
+// debug pause do frame
+void pause_debug_do();
+
+// debug pause close
+void pause_debug_close();
+
+enum
+{
+	PAUSE_TYPE_NORMAL,
+	PAUSE_TYPE_VIEWER,
+};
+
+void pause_set_type(int type);
+int pause_get_type();
+
+#endif
