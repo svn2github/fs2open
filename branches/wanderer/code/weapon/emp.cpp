svn2github/fs2open

commit 1d044daeab828b23bc619e162c116bc7ac74aa8b

option for setting emp resistance
git-svn-id: svn://svn.icculus.org/fs2open@5059 387891d4-d844-0410-90c0-e4c51a9137d3

branches/wanderer/code/weapon/emp.cpp
@@ -304,7 +304,13 @@ void emp_apply(vec3d *pos, float inner_radius, float outer_radius, float emp_int
 					scale_factor = 1.0f;
 					if(dist_mag >= inner_radius){
 						scale_factor = 1.0f - (dist_mag / outer_radius);
-					} 
+					}
+
+					scale_factor -= Ship_info[Ships[target->instance].ship_info_index].emp_resistance_mod;
+
+					if (scale_factor < 0.0f) {
+						continue;
+					}
 					
 					// disrupt the turret
 					ship_subsys_set_disrupted(moveup, (int)(MAX_TURRET_DISRUPT_TIME * scale_factor));
@@ -330,10 +336,14 @@ void emp_apply(vec3d *pos, float inner_radius, float outer_radius, float emp_int
 			// compute a scale factor for the emp effect
 			scale_factor = 1.0f;
 			if(dist_mag >= inner_radius){
-				scale_factor = 1.0f - (dist_mag / outer_radius);
-				actual_intensity = emp_intensity * scale_factor;
-				actual_time = emp_time * scale_factor;		
+				scale_factor = 1.0f - (dist_mag / outer_radius);	
 			} 
+
+			scale_factor -= Ship_info[Ships[target->instance].ship_info_index].emp_resistance_mod;
+
+			if (scale_factor < 0.0f) {
+				continue;
+			}
 		
 			// calculate actual EMP effect values
 			actual_intensity = emp_intensity * scale_factor;

branches/wanderer/code/ship/ship.cpp
@@ -3086,6 +3086,8 @@ void init_ship_entry(ship_info *sip)
 	sip->radar_projection_size_mult = 1.0f;
 
 	sip->num_shield_segments = 4;
+
+	sip->emp_resistance_mod = 0.0f;
 }
 
 // function to parse the information for a specific ship type.	
@@ -4703,6 +4705,10 @@ strcpy(parse_error_text, temp_error);
 				}
 			}
 		}
+	}
+
+	if (optional_string("$EMP Resistance Modifier:")) {
+		stuff_float(&sip->emp_resistance_mod);
 	}
 	
 	int n_subsystems = 0;

branches/wanderer/code/ship/ship.h
@@ -2038,6 +2038,8 @@ typedef struct ship_info {
 	vec3d convergence_offset;
 
 	int num_shield_segments;
+
+	float emp_resistance_mod;
 } ship_info;
 
 extern int Num_wings;

commit 333827e795189babae0598508b28bd42a1b21c78

copied from wrong patch... fix possible endless loop caused by r5059
git-svn-id: svn://svn.icculus.org/fs2open@5060 387891d4-d844-0410-90c0-e4c51a9137d3

branches/wanderer/code/weapon/emp.cpp
@@ -309,6 +309,7 @@ void emp_apply(vec3d *pos, float inner_radius, float outer_radius, float emp_int
 					scale_factor -= Ship_info[Ships[target->instance].ship_info_index].emp_resistance_mod;
 
 					if (scale_factor < 0.0f) {
+						moveup = moveup->next;
 						continue;
 					}
 					