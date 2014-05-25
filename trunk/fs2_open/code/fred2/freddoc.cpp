svn2github/fs2open

commit ae9ce7eba6d63e32908e904a68875436bae6bdb8

update FSM import for FSPort v3.0 --Goober5000
git-svn-id: svn://svn.icculus.org/fs2open@2768 387891d4-d844-0410-90c0-e4c51a9137d3

trunk/fs2_open/code/fred2/freddoc.cpp
@@ -9,9 +9,9 @@
 
 /*
  * $Logfile: /Freespace2/code/Fred2/FREDDoc.cpp $
- * $Revision: 1.2 $
- * $Date: 2006-01-30 06:27:59 $
- * $Author: taylor $
+ * $Revision: 1.3 $
+ * $Date: 2006-01-31 01:53:36 $
+ * $Author: Goober5000 $
  *
  * FREDDoc.cpp : implementation of the CFREDDoc class
  * Document class for document/view architechure, which we don't really use in
@@ -19,6 +19,9 @@
  * mainly.  Most of the MFC related stuff is handled in FredView.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2006/01/30 06:27:59  taylor
+ * dynamic starfield bitmaps
+ *
  * Revision 1.1  2006/01/19 02:27:31  Goober5000
  * import FRED2 back into fs2_open module
  * --Goober5000
@@ -629,7 +632,7 @@ int CFREDDoc::autoload()
 }
 
 // read in a new mission file from disk
-int CFREDDoc::load_mission(char *pathname, int importFSM)
+int CFREDDoc::load_mission(char *pathname, int flags)
 {
 	// make sure we're in the correct working directory!!!!!!
 	chdir(Fred_base_dir);
@@ -649,8 +652,9 @@ int CFREDDoc::load_mission(char *pathname, int importFSM)
 	clear_mission();
 
 	Fred_found_unknown_ship_during_parsing = 0;
-	if (parse_main(pathname, 0, importFSM)) {
-		if (importFSM)
+	if (parse_main(pathname, flags))
+	{
+		if (flags & MPF_IMPORT_FSM)
 		{
 			Fred_view_wnd->MessageBox("Import failed.");
 		}
@@ -1168,7 +1172,7 @@ void CFREDDoc::OnFileImportFSM()
 	// load mission into memory
 	if (Briefing_dialog)
 		Briefing_dialog->icon_select(-1);  // clean things up first
-	if (load_mission(fs1_mission, 1))
+	if (load_mission(fs1_mission, MPF_IMPORT_FSM))
 		return;
 
 	// ugh - change the file name

trunk/fs2_open/code/fred2/freddoc.h
@@ -9,8 +9,8 @@
 
 /*
  * $Logfile: /Freespace2/code/FRED2/FREDDoc.h $
- * $Revision: 1.1 $
- * $Date: 2006-01-19 02:27:31 $
+ * $Revision: 1.2 $
+ * $Date: 2006-01-31 01:53:36 $
  * $Author: Goober5000 $
  *
  * FREDDoc.h : interface of the CFREDDoc class
@@ -19,6 +19,10 @@
  * mainly.  Most of the MFC related stuff is handled in FredView.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2006/01/19 02:27:31  Goober5000
+ * import FRED2 back into fs2_open module
+ * --Goober5000
+ *
  * Revision 1.8  2005/07/13 02:40:50  Goober5000
  * remove PreProcDefine #includes in FRED
  * --Goober5000
@@ -117,7 +121,7 @@ protected: // create from serialization only
 public:
 	int check_undo();
 	int autoload();
-	int load_mission(char *pathname, int importFSM = 0);
+	int load_mission(char *pathname, int flags = 0);
 	int autosave(char *desc);
 	int save_matrix(matrix &m, FILE *fp);
 	int save_vector(vec3d &v, FILE *fp);

trunk/fs2_open/code/mission/missionparse.cpp
@@ -9,13 +9,16 @@
 
 /*
  * $Logfile: /Freespace2/code/Mission/MissionParse.cpp $
- * $Revision: 2.142 $
- * $Date: 2006-01-30 19:35:41 $
- * $Author: taylor $
+ * $Revision: 2.143 $
+ * $Date: 2006-01-31 01:53:37 $
+ * $Author: Goober5000 $
  *
  * main upper level code for parsing stuff
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.142  2006/01/30 19:35:41  taylor
+ * fix parsing of reinforcements
+ *
  * Revision 2.141  2006/01/30 06:30:03  taylor
  * dynamic starfield bitmaps
  *
@@ -1227,6 +1230,15 @@ int parse_object_on_arrival_list(p_object *pobjp);
 void mission_parse_mark_non_arrival(p_object *p_objp);
 void mission_parse_mark_non_arrival(wing *wingp);
 void mission_parse_mark_non_arrivals();
+
+// Goober5000 - FRED import
+void convertFSMtoFS2();
+void conv_fix_briefing_stuff();
+void conv_fix_punctuation();
+void conv_fix_music();
+void restore_default_weapons(char *ships_tbl);
+void restore_one_primary_bank(int *ship_primary_weapons, int *default_primary_weapons);
+void restore_one_secondary_bank(int *ship_secondary_weapons, int *default_secondary_weapons);
 
 
 MONITOR(NumShipArrivals);
@@ -4850,7 +4862,7 @@ void parse_mission(mission *pm, int flag)
 
 	Current_file_checksum = netmisc_calc_checksum(pm,MISSION_CHECKSUM_SIZE);
 
-	if ( flag == MISSION_PARSE_MISSION_INFO )
+	if (flag & MPF_ONLY_MISSION_INFO)
 		return;
 
 	parse_plot_info(pm);
@@ -5179,7 +5191,7 @@ void parse_init()
 // to get when parsing the mission.  0 means get everything (default).  Other flags just gets us basic
 // info such as game type, number of players etc.
 // Goober5000 - allow for import
-int parse_main(char *mission_name, int flags, int importFSM)
+int parse_main(char *mission_name, int flags)
 {
 	int rval, i;
 
@@ -5200,7 +5212,7 @@ int parse_main(char *mission_name, int flags, int importFSM)
 		lcl_ext_open();
 
 		// don't do this for imports
-		if (!importFSM)
+		if (!(flags & MPF_IMPORT_FSM))
 		{
 			CFILE *ftemp = cfopen(mission_name, "rt", CFILE_NORMAL, CF_TYPE_MISSIONS);
 
@@ -5223,7 +5235,7 @@ int parse_main(char *mission_name, int flags, int importFSM)
 		}
 
 		// import?
-		if (importFSM)
+		if (flags & MPF_IMPORT_FSM)
 		{
 			read_file_text(mission_name, CF_TYPE_ANY);
 			convertFSMtoFS2();
@@ -5588,7 +5600,7 @@ int mission_parse_is_multi(char *filename, char *mission_name)
 
 int mission_parse_get_multi_mission_info( char *filename )
 {
-	if ( parse_main(filename, MISSION_PARSE_MISSION_INFO) ){
+	if ( parse_main(filename, MPF_ONLY_MISSION_INFO) ){
 		return -1;
 	}
 
@@ -6884,129 +6896,14 @@ int is_training_mission()
 // Goober5000
 void convertFSMtoFS2()
 {
-	// replace class names
-	conv_replace_ship_classes();
-
-	// add alt names
-	conv_add_alt_names();
-
 	// fix icons
 	conv_fix_briefing_stuff();
 
 	// fix punctuation
 	conv_fix_punctuation();
-}
 
-// Goober5000 - replacement rules according to the Freespace Port
-void conv_replace_ship_classes()
-{
-	// only replace the Terran NavBuoy if it isn't found in the tables
-	// (even if we do replace it, it's still alt-named as a Terran NavBuoy... see below)
-	if (ship_info_lookup("Terran NavBuoy") < 0)
-	{
-		replace_all(Mission_text, "lass: Terran NavBuoy", "lass: GTNB Pharos", MISSION_TEXT_SIZE);
-	}
-
-	// only replace the Prometheus if it isn't found in the tables
-	if (weapon_info_lookup("Prometheus") < 0)
-	{
-		replace_all(Mission_text, "\"Prometheus\"", "\"Prometheus S\"", MISSION_TEXT_SIZE);
-	}
-
-	// change most Vasudan stuff from PV to GV
-	replace_all(Mission_text, "lass: PV", "lass: GV", MISSION_TEXT_SIZE);
-	replace_all(Mission_text, "GVF Anubis", "PVF Anubis", MISSION_TEXT_SIZE);
-	replace_all(Mission_text, "GVB Amun", "PVB Amun", MISSION_TEXT_SIZE);
-	replace_all(Mission_text, "GVFr Ma'at", "PVFr Ma'at", MISSION_TEXT_SIZE);
-	replace_all(Mission_text, "GVFr Bast", "PVFr Bast", MISSION_TEXT_SIZE);
-}
-
-// Goober5000
-void conv_add_alt_names()
-{
-	int flag[MAX_SHIP_CLASSES];
-	int i, idx, alt_tally;
-	char *ch;
-	char ship_class[NAME_LENGTH * 2];
-	char alt_text[NAME_LENGTH * 2 + 10];
-	char alt_section[MAX_ALT_TYPE_NAMES * (NAME_LENGTH + 7) + 35];
-
-	// initialize stuff
-	ch = Mission_text;
-	memset(flag, 0, sizeof(int) * MAX_SHIP_CLASSES);
-	alt_tally = 0;
-
-	// get all ship classes (case sensitive, because lowercase
-	// $class is for briefing icons)
-	while ((ch = strstr(ch + 1, "$Class:")) != NULL)
-	{
-		// extract ship class
-		copy_to_eoln(ship_class, NULL, ch + 7, NAME_LENGTH * 2);
-		drop_white_space(ship_class);
-
-		// handle $alt
-		*alt_text = 0;
-
-		// see if it's Vasudan
-		if (!strnicmp(ship_class, "GV", 2))
-		{
-			if ((idx = ship_info_lookup(ship_class)) >= 0)
-			{
-				// flag for inclusion at the top
-				flag[idx] = 1;
-
-				// add $alt text below ship class
-				sprintf(alt_text, "%s\n$Alt: P%s", ship_class, ship_class + 1);
-			}
-		}
-		// see if it's the navbuoy
-		else if (!stricmp(ship_class, "GTNB Pharos"))
-		{
-			// flag for inclusion
-			flag[ship_info_lookup(ship_class)] = 1;
-
-			// add $alt text below ship class
-			sprintf(alt_text, "GTNB Pharos\n$Alt: Terran NavBuoy");
-		}
-
-		// now actually replace the class with the class plus $alt text
-		if (*alt_text)
-		{
-			replace_one(ch, ship_class, alt_text, MISSION_TEXT_SIZE);
-			alt_tally++;
-		}	
-	}
-
-	// do we get to add the section?
-	if (alt_tally)
-	{
-		// set up the $alt section
-		strcpy(alt_section, "#Alternate Types:\n");
-
-		// find which classes need to be added
-		for (i = 0; i < MAX_SHIP_CLASSES; i++)
-		{
-			if (flag[i])
-			{
-				// Vasudan?
-				if (!strnicmp(Ship_info[i].name, "GV", 2))
-				{
-					strcat(alt_section, "$Alt: P");
-					strcat(alt_section, Ship_info[i].name + 1);
-					strcat(alt_section, "\n");
-				}
-				// navbuoy?
-				else if (!stricmp(Ship_info[i].name, "GTNB Pharos"))
-				{
-					strcat(alt_section, "$Alt: Terran NavBuoy\n");
-				}
-			}
-		}
-		strcat(alt_section, "#end\n\n\n#Players");
-
-		// add it (it goes right before the #Players section)
-		replace_one(Mission_text, "#Players", alt_section, MISSION_TEXT_SIZE);
-	}
+	// fix music
+	conv_fix_music();
 }
 
 // Goober5000
@@ -7023,7 +6920,7 @@ void conv_fix_briefing_stuff()
 }
 
 // Goober5000
-// go through all the displayed text in one section and fix " and ;
+// go through all the displayed text in one section and fix "
 // the section and text delimiters should all be different
 void conv_fix_punctuation_section(char *str, char *section_start, char *section_end, char *text_start, char *text_end)
 {
@@ -7043,7 +6940,6 @@ void conv_fix_punctuation_section(char *str, char *section_start, char *section_
 		if (!t2 || t2 > s2) return;
 
 		replace_all(t1, "\"", "$quote", MISSION_TEXT_SIZE, (t2 - t1));
-		replace_all(t1, ";", "$semicolon", MISSION_TEXT_SIZE, (t2 - t1));
 	}	
 }
 
@@ -7061,6 +6957,34 @@ void conv_fix_punctuation()
 
 	// messages
 	conv_fix_punctuation_section(Mission_text, "#Messages", "#Reinforcements", "$Message:", "\n");
+}
+
+// Goober5000
+void conv_fix_music()
+{
+	char *ch;
+	char *track_ch;
+	char name[NAME_LENGTH];
+	char new_name[NAME_LENGTH];
+
+	// skip to music section
+	ch = strstr(Mission_text, "#Music");
+	if (!ch) return;
+
+	// skip to event music
+	ch = strstr(ch, "$Event Music:");
+	if (!ch) return;
+
+	// get music track name
+	track_ch = ch + 14;
+	copy_to_eoln(name, NULL, track_ch, NAME_LENGTH);
+
+	// form new name
+	strcpy(new_name, "FS1-");
+	strcpy(new_name + 4, name);
+
+	// replace it
+	replace_all(Mission_text, name, new_name, MISSION_TEXT_SIZE);
 }
 
 // Goober5000
@@ -7088,19 +7012,8 @@ void restore_default_weapons(char *ships_tbl)
 			// get ship_info
 			sip = &Ship_info[Ships[i].ship_info_index];
 
-			// get the ship class and adjust as necessary
-			strcpy(ship_class, sip->name);
-			if (!strnicmp(ship_class, "GV", 2))
-			{
-				*ship_class = 'P';
-			}
-			else if (!stricmp(ship_class, "GTNB Pharos"))
-			{
-				strcpy(ship_class, "Terran NavBuoy");
-			}
-
 			// find the ship class
-			ch = stristr(ships_tbl, ship_class);
+			ch = strstr(ships_tbl, ship_class);
 			if (!ch) continue;
 
 			// check pbanks (capital ships have these specified but empty)
@@ -7166,15 +7079,6 @@ void restore_one_primary_bank(int *ship_primary_weapons, int *default_primary_we
 
 	// stuff weapon list
 	count = stuff_string_list(weapon_list, MAX_SHIP_PRIMARY_BANKS);
-
-	// possibly replace Prometheus with Prometheus S
-	for (i = 0; i < count; i++)
-	{
-		if (!stricmp(weapon_list[i], "Prometheus"))
-		{
-			strcpy(weapon_list[i], "Prometheus S");
-		}
-	}
 
 	// check for default weapons - if same as default, overwrite with the one from the table
 	for (i = 0; i < count; i++)

trunk/fs2_open/code/mission/missionparse.h
@@ -9,13 +9,16 @@
 
 /*
  * $Source: /cvs/cvsroot/fs2open/fs2_open/code/mission/missionparse.h,v $
- * $Revision: 2.76 $
- * $Author: wmcoolmon $
- * $Date: 2006-01-14 19:54:55 $
+ * $Revision: 2.77 $
+ * $Author: Goober5000 $
+ * $Date: 2006-01-31 01:53:37 $
  *
  * main header file for parsing code  
  *
  * $Log: not supported by cvs2svn $
+ * Revision 2.76  2006/01/14 19:54:55  wmcoolmon
+ * Special shockwave and moving capship bugfix, (even more) scripting stuff, slight rearrangement of level management functions to facilitate scripting access.
+ *
  * Revision 2.75  2006/01/13 03:31:09  Goober5000
  * übercommit of custom IFF stuff :)
  *
@@ -515,8 +518,9 @@ struct p_dock_instance;
 
 #define WING_PLAYER_BASE	0x80000  // used by Fred to tell ship_index in a wing points to a player
 
-// defines used for parse_mission() to tell what kind of information to get from the mission file
-#define MISSION_PARSE_MISSION_INFO	1
+// mission parse flags used for parse_mission() to tell what kind of information to get from the mission file
+#define MPF_ONLY_MISSION_INFO	(1 << 0)
+#define MPF_IMPORT_FSM			(1 << 1)
 
 // bitfield definitions for missions game types
 #define OLD_MAX_GAME_TYPES				4					// needed for compatibility
@@ -878,7 +882,7 @@ extern char Neb2_texture_name[MAX_FILENAME_LEN];
 #define FRED_DUPLICATE_MODEL_TEXTURE_REPLACE	2
 extern texture_replace Fred_texture_replacements[MAX_SHIPS * MAX_MODEL_TEXTURES];
 
-int parse_main(char *mission_name, int flags = 0, int importFSM = 0);
+int parse_main(char *mission_name, int flags = 0);
 p_object *mission_parse_get_arrival_ship(ushort net_signature);
 p_object *mission_parse_get_arrival_ship(char *name);
 p_object *mission_parse_get_parse_object(ushort net_signature);
@@ -925,16 +929,6 @@ void mission_parse_reset_alt();
 
 // code to save/restore mission parse stuff
 int get_mission_info(char *filename, mission *missionp = NULL);
-
-// Goober5000 - FRED import
-void convertFSMtoFS2();
-void conv_replace_ship_classes();
-void conv_add_alt_names();
-void conv_fix_briefing_stuff();
-void conv_fix_punctuation();
-void restore_default_weapons(char *ships_tbl);
-void restore_one_primary_bank(int *ship_primary_weapons, int *default_primary_weapons);
-void restore_one_secondary_bank(int *ship_secondary_weapons, int *default_secondary_weapons);
 
 // Goober5000
 void parse_dock_one_docked_object(p_object *pobjp, p_object *parent_pobjp);

commit 1a832a77bac7791d4fe5b9ed07d4e84176656fd2

remove Import Weapon Loadouts feature, which was mainly used for versions of FSPort prior to 3.0 and was broken from r2768 to r10718 anyway
git-svn-id: svn://svn.icculus.org/fs2open@10720 387891d4-d844-0410-90c0-e4c51a9137d3

trunk/fs2_open/code/fred2/fred.rc
@@ -171,7 +171,6 @@ BEGIN
         POPUP "&Import"
         BEGIN
             MENUITEM "&FreeSpace 1 mission...",     33074
-            MENUITEM "&Weapon loadouts...",         33075
         END
         MENUITEM SEPARATOR
         MENUITEM "&Run FreeSpace\tAlt+R",       32985

trunk/fs2_open/code/fred2/freddoc.cpp
@@ -79,7 +79,6 @@ BEGIN_MESSAGE_MAP(CFREDDoc, CDocument)
 	ON_COMMAND(ID_EDIT_UNDO, OnEditUndo)
 	ON_COMMAND(ID_FILE_PREFERENCES, OnFilePreferences)
 	ON_COMMAND(ID_FILE_IMPORT_FSM, OnFileImportFSM)
-	ON_COMMAND(ID_FILE_IMPORT_WEAPONS, OnFileImportWeapons)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
@@ -1013,89 +1012,6 @@ void CFREDDoc::OnFileImportFSM()
 
 	MessageBox(NULL, "Import complete.  Please check the destination folder to verify all missions were imported successfully.", "Status", MB_OK);
 	recreate_dialogs();
-}
-
-void restore_default_weapons(char *ships_tbl);
-
-void CFREDDoc::OnFileImportWeapons() 
-{
-	int rval;
-	CString ships_tbl_mfc;
-	char ships_tbl[MAX_PATH_LEN];
-	char *ships_tbl_text;
-	char *ships_tbl_text_raw;
-	char *ch;
-	char temp[MAX_PATH_LEN];
-	char error_str[1024];
-
-	// set up import dialog
-	CFileDialog dlg(TRUE, "tbl", NULL, OFN_HIDEREADONLY | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR, "FreeSpace Tables (*.tbl)|*.tbl|All files (*.*)|*.*||");
-	dlg.m_ofn.lpstrTitle = "Specify the ships.tbl with the required loadouts";
-
-	// set initial path
-	strcpy_s(temp, Fred_exe_dir);
-	if ((ch = stristr(temp, "FreeSpace2")) != NULL)
-	{
-		strcpy(ch, "FreeSpace\\Data\\Tables");
-		dlg.m_ofn.lpstrInitialDir = temp;
-	}
-
-	// get ships.tbl
-	if (dlg.DoModal() != IDOK)
-		return;
-
-	ships_tbl_mfc = dlg.GetPathName();
-
-	if (strlen(ships_tbl_mfc) > MAX_PATH_LEN - 1)
-	{
-		MessageBox(NULL, "Path name is too long", "Error", MB_OK);
-		return;
-	}
-
-	if (!strlen(ships_tbl_mfc))
-		return;
-
-	strcpy_s(ships_tbl, ships_tbl_mfc);
-
-	// allocate junk
-	ships_tbl_text = (char *) malloc(sizeof(char) * MISSION_TEXT_SIZE);
-	ships_tbl_text_raw = (char *) malloc(sizeof(char) * MISSION_TEXT_SIZE);
-	if (!ships_tbl_text || !ships_tbl_text_raw)
-	{
-		free(ships_tbl_text);
-		free(ships_tbl_text_raw);
-
-		Error(LOCATION, "Not enough memory to import weapons.");
-		return;
-	}
-
-	// open localization
-	lcl_ext_open();
-
-	if ((rval = setjmp(parse_abort)) != 0) {
-		mprintf(("FREDDOC: Unable to parse '%s'!  Error code = %i.\n", ships_tbl, rval));
-		sprintf(error_str, "Could not parse file: %s", ships_tbl);
-
-		MessageBox(NULL, error_str, "Unable to import weapon loadouts!", MB_ICONERROR | MB_OK);
-		lcl_ext_close();
-		return;
-	}
-
-	// load the other table and convert, freeing memory after use
-	read_file_text(ships_tbl, CF_TYPE_ANY, ships_tbl_text, ships_tbl_text_raw);
-	free(ships_tbl_text_raw);
-	restore_default_weapons(ships_tbl_text);
-	free(ships_tbl_text);
-
-	// close localization
-	lcl_ext_close();
-
-	// we haven't saved it yet
-	set_modified(TRUE);
-
-	// error check and notify
-	if (!Fred_view_wnd->global_error_check())
-		Fred_view_wnd->MessageBox("Weapon loadouts successfully imported with no errors.", "Woohoo!");
 }
 
 void CFREDDoc::recreate_dialogs()

trunk/fs2_open/code/fred2/freddoc.h
@@ -89,7 +89,6 @@ protected:
 	afx_msg void OnFileNew();
 	afx_msg void editor_init_mission();
 	afx_msg void OnFileImportFSM();
-	afx_msg void OnFileImportWeapons();
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
 

trunk/fs2_open/code/fred2/resource.h
@@ -1439,7 +1439,6 @@
 #define ID_FORMAT_FS1_RETAIL            33072
 #define ID_EDITORS_SET_GLOBAL_SHIP_FLAGS 33073
 #define ID_FILE_IMPORT_FSM              33074
-#define ID_FILE_IMPORT_WEAPONS          33075
 #define ID_VIEW_LIGHTING                33079
 #define ID_EDITORS_VOICE                33080
 #define ID_SHOW_IFF_0                   33081

trunk/fs2_open/code/mission/missionparse.cpp
@@ -393,9 +393,6 @@ void mission_parse_mark_non_arrivals();
 
 // Goober5000 - FRED import
 void convertFSMtoFS2();
-void restore_default_weapons(char *ships_tbl);
-void restore_one_primary_bank(int *ship_primary_weapons, int *default_primary_weapons);
-void restore_one_secondary_bank(int *ship_secondary_weapons, int *default_secondary_weapons);
 
 
 MONITOR(NumShipArrivals)
@@ -7920,131 +7917,6 @@ void convertFSMtoFS2()
 {
 	// fix punctuation
 	conv_fix_punctuation();
-}
-
-// Goober5000
-void restore_default_weapons(char *ships_tbl)
-{
-	int i, j, si_subsys;
-	char *ch, *subsys;
-	char ship_class[NAME_LENGTH];
-	ship_subsys *ss;
-	ship_info *sip;
-
-	// guesstimate that this actually is a ships.tbl
-	if (!strstr(ships_tbl, "#Ship Classes"))
-	{
-		MessageBox(NULL, "This is not a ships.tbl file.  Aborting conversion...", "Error", MB_OK);
-		return;
-	}
-
-	// for every ship
-	for (i = 0; i < MAX_SHIPS; i++)
-	{
-		// ensure the ship slot is used in this mission
-		if (Ships[i].objnum >= 0)
-		{
-			// get ship_info
-			sip = &Ship_info[Ships[i].ship_info_index];
-			strcpy_s(ship_class, sip->name);
-
-			// find the ship class
-			ch = strstr(ships_tbl, ship_class);
-			if (!ch) continue;
-
-			// check pbanks (capital ships have these specified but empty)
-			Mp = strstr(ch, "$Default PBanks");
-			Mp = strchr(Mp, '(');
-			restore_one_primary_bank(Ships[i].weapons.primary_bank_weapons, sip->primary_bank_weapons);
-
-			// check sbanks (capital ships have these specified but empty)
-			Mp = strstr(ch, "$Default SBanks");
-			Mp = strchr(Mp, '(');
-			restore_one_secondary_bank(Ships[i].weapons.secondary_bank_weapons, sip->secondary_bank_weapons);
-
-			// see if we have any turrets
-			ch = strstr(ch, "$Subsystem");
-			for (ss = GET_FIRST(&Ships[i].subsys_list); ss != END_OF_LIST(&Ships[i].subsys_list); ss = GET_NEXT(ss))
-			{
-				// we do
-				if (ss->system_info->type == SUBSYSTEM_TURRET)
-				{
-					// find it in the ship_info subsys list
-					si_subsys = -1;
-					for (j = 0; j < sip->n_subsystems; j++)
-					{
-						if (!subsystem_stricmp(ss->system_info->subobj_name, sip->subsystems[j].subobj_name))
-						{
-							si_subsys = j;
-							break;
-						}
-					}
-					if (si_subsys < 0) continue;
-
-					// find it in the file - make sure it belongs to *this* ship
-					subsys = stristr(ch, ss->system_info->subobj_name);
-					if (!subsys) continue;
-					if (subsys > strstr(ch, "$Name")) continue;
-
-					// check pbanks - make sure they are *this* subsystem's banks
-					Mp = strstr(subsys, "$Default PBanks");
-					if (Mp < strstr(subsys + 1, "$Subsystem"))
-					{
-						Mp = strchr(Mp, '(');
-						restore_one_primary_bank(ss->weapons.primary_bank_weapons, sip->subsystems[si_subsys].primary_banks);
-					}
-
-					// check sbanks - make sure they are *this* subsystem's banks
-					Mp = strstr(subsys, "$Default SBanks");
-					if (Mp < strstr(subsys + 1, "$Subsystem"))
-					{
-						Mp = strchr(Mp, '(');
-						restore_one_secondary_bank(ss->weapons.secondary_bank_weapons, sip->subsystems[si_subsys].secondary_banks);
-					}
-				}
-			}
-		}
-	}
-}
-
-// Goober5000
-void restore_one_primary_bank(int *ship_primary_weapons, int *default_primary_weapons)
-{
-	int i, count, original_weapon;
-	char weapon_list[MAX_SHIP_PRIMARY_BANKS][NAME_LENGTH];
-
-	// stuff weapon list
-	count = stuff_string_list(weapon_list, MAX_SHIP_PRIMARY_BANKS);
-
-	// check for default weapons - if same as default, overwrite with the one from the table
-	for (i = 0; i < count; i++)
-	{
-		if (ship_primary_weapons[i] == default_primary_weapons[i])
-		{
-			if ((original_weapon = weapon_info_lookup(weapon_list[i])) >= 0)
-			ship_primary_weapons[i] = original_weapon;
-		}
-	}
-}
-
-// Goober5000
-void restore_one_secondary_bank(int *ship_secondary_weapons, int *default_secondary_weapons)
-{
-	int i, count, original_weapon;
-	char weapon_list[MAX_SHIP_SECONDARY_BANKS][NAME_LENGTH];
-
-	// stuff weapon list
-	count = stuff_string_list(weapon_list, MAX_SHIP_SECONDARY_BANKS);
-
-	// check for default weapons - if same as default, overwrite with the one from the table
-	for (i = 0; i < count; i++)
-	{
-		if (ship_secondary_weapons[i] == default_secondary_weapons[i])
-		{
-			if ((original_weapon = weapon_info_lookup(weapon_list[i])) >= 0)
-			ship_secondary_weapons[i] = original_weapon;
-		}
-	}
 }
 
 void clear_texture_replacements() 