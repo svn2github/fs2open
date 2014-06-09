svn2github/fs2open

commit 5e685fd77381bc0043b3e0f329bed5b523593685

Don't parse bsp trees of non-colliding submodels.
git-svn-id: svn://svn.icculus.org/fs2open@10729 387891d4-d844-0410-90c0-e4c51a9137d3

branches/zookeeper/code/model/modelread.cpp
@@ -2674,7 +2674,8 @@ int model_load(char *filename, int n_subsystems, model_subsystem *subsystems, in
 			pm->submodel[i].collision_tree_index = model_create_bsp_collision_tree();
 			bsp_collision_tree *tree = model_get_bsp_collision_tree(pm->submodel[i].collision_tree_index);
 
-			model_collide_parse_bsp(tree, pm->submodel[i].bsp_data, pm->version);
+			if ( !(pm->submodel[i].nocollide_this_only || pm->submodel[i].no_collisions) )
+				model_collide_parse_bsp(tree, pm->submodel[i].bsp_data, pm->version);
 		}
 	}
 

commit 8f09199ea1a6511c882bb629f05e84ccf1811d52

Fixed crash due to an improperly placed if, introduced in r10729.
git-svn-id: svn://svn.icculus.org/fs2open@10790 387891d4-d844-0410-90c0-e4c51a9137d3

branches/zookeeper/code/model/modelread.cpp
@@ -2678,11 +2678,11 @@ int model_load(char *filename, int n_subsystems, model_subsystem *subsystems, in
 
 	if ( !Cmdline_old_collision_sys ) {
 		for ( i = 0; i < pm->n_models; ++i ) {
-			pm->submodel[i].collision_tree_index = model_create_bsp_collision_tree();
-			bsp_collision_tree *tree = model_get_bsp_collision_tree(pm->submodel[i].collision_tree_index);
-
-			if ( !(pm->submodel[i].nocollide_this_only || pm->submodel[i].no_collisions) )
+			if ( !(pm->submodel[i].nocollide_this_only || pm->submodel[i].no_collisions) ) {
+				pm->submodel[i].collision_tree_index = model_create_bsp_collision_tree();
+				bsp_collision_tree *tree = model_get_bsp_collision_tree(pm->submodel[i].collision_tree_index);
 				model_collide_parse_bsp(tree, pm->submodel[i].bsp_data, pm->version);
+			}
 		}
 	}
 